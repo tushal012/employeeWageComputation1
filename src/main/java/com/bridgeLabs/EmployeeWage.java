cd23503f83d7050abb2d975fbef158fe061ecec2
tushal012 <tushalkashyap11@gmail.com>
2021-05-21 20:44:30 +0530

    Added: daily wage features of employee Wage Computation

--- src/main/java/com/bridgeLabs/EmployeeWage.java
+++ src/main/java/com/bridgeLabs/EmployeeWage.java
@@ -4,11 +4,17 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation");
         int IS_PRESENT = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == IS_PRESENT)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is Absent");
+        int  EMP_RATE_PER_HR=20;
+        int EMP_HR=8;
+        int salary=0;
 
+
+        double randomCheck = Math.floor(Math.random() * 10) % 2;
+        if(randomCheck == IS_PRESENT) {
+            System.out.println("salary is"+" "+EMP_RATE_PER_HR * EMP_HR);
+        } else {
+            salary = 0;
+            System.out.println("Employee is absent" + " " + salary);
+        }
     }
 }
