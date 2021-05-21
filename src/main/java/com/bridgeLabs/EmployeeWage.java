72deac33c8f0a29072a3495472c5298e4b630539
tushal012 <tushalkashyap11@gmail.com>
2021-05-21 21:06:24 +0530

    Added: Monthly Wage Calculation for employeeWageComputation

--- src/main/java/com/bridgeLabs/EmployeeWage.java
+++ src/main/java/com/bridgeLabs/EmployeeWage.java
@@ -7,22 +7,26 @@ public class EmployeeWage {
         int IS_PART_TIME = 1;
         int IS_FULL_TIME = 2;
         int  EMP_RATE_PER_HR=20;
+        int WORKING_HOURS=20;
         int EMP_HR =0;
         int salary=0;
+        int MONTHLY_SALARY=0;
 
+        for (int i = 1; i <= WORKING_HOURS; i++) {
+            double randomCheck = Math.floor(Math.random() * 10) % 3;
+            switch ((int) randomCheck) {
+                case 1:
+                    EMP_HR = 4;
+                    break;
+                case 2:
+                    EMP_HR = 8;
+                    break;
+                case 3:
+                    EMP_HR = 0;
 
-        double randomCheck = Math.floor(Math.random() * 10) % 2;
-        switch ((int) randomCheck) {
-            case 1:
-                EMP_HR = 4;
-                break;
-            case 2:
-                EMP_HR = 8;
-                break;
-            case 3:
-                EMP_HR = 0;
+            }
+            salary = EMP_RATE_PER_HR * EMP_HR;
+            System.out.println(salary);
         }
-        salary = EMP_RATE_PER_HR * EMP_HR;
-        System.out.println(salary);
     }
 }
